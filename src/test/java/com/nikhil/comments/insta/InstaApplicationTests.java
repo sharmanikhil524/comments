8b5eb80d9f3b38b3d61a6e7ebddb4d13fe372f7e
unknown <sharmanikhil524@gmail.com>
2024-02-05 16:33:34 +0530

    by nikhil comments

---
+++ src/test/java/com/nikhil/comments/insta/InstaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.comments.insta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
